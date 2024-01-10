2cf61541abc904756d04c26ccdd1fc4e57996302
BudakCS255 <148829262+BudakCS255@users.noreply.github.com>
2024-01-10 08:33:47 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="verify.php" method="post">
+        <label>Email:</label>
+        <input type="email" name="email" required>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
